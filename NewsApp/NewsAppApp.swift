214aa1b8ac215fcfc696a9c39890e39f716d2778
sedtyldz <ikinciyargic@gmail.com>
2024-09-11 21:35:31 +0300

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Sedat Yıldız on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
